ecbfcb964e39cc081ba50d32934ae58f00b0748e
joshuagthatcher <racemts@byu.edu>
2024-02-10 00:38:59 -0700

    joshua

---
+++ MissionFive_Thatcher/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MissionFive_Thatcher.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
